27404c38b570b118cb7cde7cecdda7e7ed818f0c
Sarathbaiju <user@DESKTOP-HDI1QAS>
2021-03-13 11:08:28 +0530

    register method added

---
+++ JWTAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using JWTAuthentication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this._userManager = userManager;
+            this._roleManager = roleManager;
+        }
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel registerViewModel)
+        {
+            try
+            {
+                if (await _userManager.FindByNameAsync(registerViewModel.UserName) != null)
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, new { status = "failed", message = "user already exist" });
+                }
+
+                var user = new IdentityUser
+                {
+                    UserName = registerViewModel.UserName,
+                    Email = registerViewModel.Email
+                };
+
+                var result = await _userManager.CreateAsync(user, registerViewModel.Password);
+
+                if (result.Succeeded)
+                {
+                    return Ok(new { status = "Success", message = "user created successfully" });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return Ok();
+        }
+    }
+}
